07f3914c6a822c0bf5b1c6b1be095b2f5a905fba
asilbekcodes <asilbeksuvonov43@gmail.com>
2024-09-26 15:40:40 +0500

    update

--- src/pages/dashboard/dashboardmas.tsx
+++ src/pages/dashboard/dashboardmas.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
+// import React, { useEffect } from 'react'
 import { FaTerminal } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { LuUserCog } from 'react-icons/lu'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import AdminHeader from '@/components/custom/AdminHeader'
+import { Button } from '@/components/ui/button'
+import Tables from '@/components/custom/Tables'
+// import { useGlobalFunction } from '@/helpers/function/global-function'
+// import { swaggerUrl } from '@/helpers/api/swagger-url'
+// import { config } from 'process'
+// import { TableTypes } from '@/helpers/interface/types'
 
 const dashboardmas: React.FC = () => {
     const { pathname } = useLocation()
+    // const RejectedMaster = useGlobalFunction(
+    //     `${swaggerUrl}api/v1/user/rejected/list`,
+    //     'get',
+    //     undefined,
+    //     config
+    // )
+    // useEffect(() => {
+    //     const fetchRejectedMaster = async () => {
+    //         await RejectedMaster.globalDataFunc();
+    //     };
+
+    //     fetchRejectedMaster();
+    // }, []);
+
+    // if (RejectedMaster.loading) {
+    //     return <p>Loading...</p>
+    // }
+    // if (RejectedMaster.error) {
+    //     return <p>error</p>
+    // }
+    // console.log(RejectedMaster.response);
+    
 
   return (
     <div>
         <AdminHeader pageName="Masters" title="Admin"/>
-        <div className='flex'>
+        <div className='flex gap-8'>
             <div>
                 <div className="w-64 bg-gray-100 h-screen">
                     <nav>
@@ -31,7 +59,16 @@ const dashboardmas: React.FC = () => {
                 </div>
             </div>
             <div>
-                
+                <div className='my-5'>
+                    <Button variant="outline" className='mr-5'>Confirmed Masters</Button>
+                    <Button variant="outline">Not Confirmed Masters</Button>
+                </div>
+                <div>
+                    {/* {RejectedMaster.response && Array.isArray(RejectedMaster.response) && RejectedMaster.response.map((item: TableTypes) =>
+                        <Tables firstName={item.firstName} lastName={item.lastName} phoneNumber={item.phoneNumber} />
+                    )} */}
+                    <Tables firstName="John" lastName="Doe" phoneNumber="1234567890"/>
+                </div>
             </div>
         </div>
     </div>
