30918a9605e59caf3556c0487d481a00fdb2e637
asilbekcodes <asilbeksuvonov43@gmail.com>
2025-01-07 14:46:47 +0500

    update

--- src/pages/Client/Favorites.jsx
+++ src/pages/Client/Favorites.jsx
@@ -40,7 +40,7 @@ function Favorites() {
         <Navbar />
       </div>
       <div>
-        <div className="h-[75vh]">
+        <div className="min-h-[75vh]">
           {favorites.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-4 lg:px-40">
               {favorites.map((stadium) => (
