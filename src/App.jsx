f08a73ba71723c30acddfbe03dc6c71de2a8eb54
asilbekcodes <asilbeksuvonov43@gmail.com>
2025-02-09 23:11:12 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import Main from "./pages/Client/Main";
-import NotFound from "./pages/Notfount";
-import About from "./pages/Client/About";
-import Map from "./pages/Client/Map";
-import Dashboards from "./pages/Admin/Dashboards";
-import Orders from "./pages/Admin/Orders";
-import Profil from "./pages/Admin/Profil";
-import ClintLogin from "./pages/auth/clintLogin";
-import ClintCod from "./pages/auth/clintCod";
-import ClintIsm_familiya from "./pages/auth/clintIsm_familiya";
-import ClintBron from "./pages/Client/clintBron";
-import OrdersPage from "./pages/Client/OrdersPage";
+import { lazy, Suspense, useEffect, useState } from "react";
 import ThemeContextProvider from "./context/ThemeContextProvider";
-import Home from "./pages/Client/Home";
-import Favorites from "./pages/Client/Favorites";
-import Profils from "./pages/Client/Profils";
-import Regester from "./pages/auth/Regester";
-import StadinAdd from "./pages/Admin/StadinAdd";
-import { useEffect, useState } from "react";
-import Statistika from "./pages/Admin/Statistika";
-import Times_Pages from "./pages/Admin/Times";
-import Times from "./components/Admin/Times";
-import History from "./pages/Admin/History";
 import Loading from "./components/Loading";
+
+const Home = lazy(() => import("./pages/Client/Home"));
+const Main = lazy(() => import("./pages/Client/Main"));
+const NotFound = lazy(() => import("./pages/Notfount"));
+const About = lazy(() => import("./pages/Client/About"));
+const Map = lazy(() => import("./pages/Client/Map"));
+const Dashboards = lazy(() => import("./pages/Admin/Dashboards"));
+const Orders = lazy(() => import("./pages/Admin/Orders"));
+const Profil = lazy(() => import("./pages/Admin/Profil"));
+const ClintLogin = lazy(() => import("./pages/auth/clintLogin"));
+const ClintCod = lazy(() => import("./pages/auth/clintCod"));
+const ClintIsmFamiliya = lazy(() => import("./pages/auth/clintIsm_familiya"));
+const ClintBron = lazy(() => import("./pages/Client/clintBron"));
+const OrdersPage = lazy(() => import("./pages/Client/OrdersPage"));
+const Favorites = lazy(() => import("./pages/Client/Favorites"));
+const Profils = lazy(() => import("./pages/Client/Profils"));
+const Register = lazy(() => import("./pages/auth/Regester"));
+const StadinAdd = lazy(() => import("./pages/Admin/StadinAdd"));
+const Statistika = lazy(() => import("./pages/Admin/Statistika"));
+const TimesPages = lazy(() => import("./pages/Admin/Times"));
+const Times = lazy(() => import("./components/Admin/Times"));
+const History = lazy(() => import("./pages/Admin/History"));
 
 function App() {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
 
   function RoleAuth() {
     if (
@@ -55,96 +55,92 @@ function App() {
     }
   }, [navigate]);
 
-  useEffect(() => {
-    setLoading(true);
-    const timer = setTimeout(() => setLoading(false), 2000);
-    return () => clearTimeout(timer);
-  }, [location]);
   return (
     <>
-      {loading && <Loading isLoading={loading} fullScreen />}
-      <Routes>
-        {/* Client page routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/about/:resultId" element={<About />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/auth/login" element={<ClintLogin />} />
-        <Route path="/clintCod" element={<ClintCod />} />
-        <Route path="/clint/register" element={<ClintIsm_familiya />} />
-        <Route path="/clintBron/:resultId" element={<ClintBron />} />
-        <Route path="/ordersPage" element={<OrdersPage />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/client/profil" element={<Profils />} />
-        <Route path="/auth/register" element={<Regester />} />
+      <Suspense fallback={<Loading isLoading={true} fullScreen />}>
+        <Routes>
+          {/* Client pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Main />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/about/:resultId" element={<About />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/auth/login" element={<ClintLogin />} />
+          <Route path="/clintCod" element={<ClintCod />} />
+          <Route path="/clint/register" element={<ClintIsmFamiliya />} />
+          <Route path="/clintBron/:resultId" element={<ClintBron />} />
+          <Route path="/ordersPage" element={<OrdersPage />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/client/profil" element={<Profils />} />
+          <Route path="/auth/register" element={<Register />} />
 
-        {/* Admin page routes wrapped with ThemeContextProvider */}
-        <Route
-          path="/dashboard"
-          element={
-            <ThemeContextProvider>
-              <Dashboards />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/statistika"
-          element={
-            <ThemeContextProvider>
-              <Statistika />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/stadionAdd"
-          element={
-            <ThemeContextProvider>
-              <StadinAdd />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/orders"
-          element={
-            <ThemeContextProvider>
-              <Orders />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/times"
-          element={
-            <ThemeContextProvider>
-              <Times_Pages />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/profil"
-          element={
-            <ThemeContextProvider>
-              <Profil />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/time_boshqarish"
-          element={
-            <ThemeContextProvider>
-              <Times />
-            </ThemeContextProvider>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ThemeContextProvider>
-              <History />
-            </ThemeContextProvider>
-          }
-        />
-      </Routes>
+          {/* Admin pages */}
+          <Route
+            path="/dashboard"
+            element={
+              <ThemeContextProvider>
+                <Dashboards />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/statistika"
+            element={
+              <ThemeContextProvider>
+                <Statistika />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/stadionAdd"
+            element={
+              <ThemeContextProvider>
+                <StadinAdd />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/orders"
+            element={
+              <ThemeContextProvider>
+                <Orders />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/times"
+            element={
+              <ThemeContextProvider>
+                <TimesPages />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/profil"
+            element={
+              <ThemeContextProvider>
+                <Profil />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/time_boshqarish"
+            element={
+              <ThemeContextProvider>
+                <Times />
+              </ThemeContextProvider>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <ThemeContextProvider>
+                <History />
+              </ThemeContextProvider>
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 }
